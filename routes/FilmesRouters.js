50de8e51eae3e7383b0a13ae1c16c5ec3836586c
Gustaxbjj <153648794+Gustaxbjj@users.noreply.github.com>
2025-06-11 09:19:59 -0300

    sds

--- routes/FilmesRouters.js
+++ routes/FilmesRouters.js
@@ -3,16 +3,27 @@ import { Filme } from '../models/Index.js';
 const router = express.Router();
 
 router.get('/',    async (_req, res) => res.json(await Filme.findAll()));
+
 router.get('/:id', async (req, res) => {
   const f = await Filme.findByPk(req.params.id);
   f ? res.json(f) : res.status(404).json({ error: 'Filme não encontrado' });
 });
-router.post('/',   async (req, res) => res.status(201).json(await Filme.create(req.body)));
+
+router.post('/',   async (req, res) => {
+
+  console.log('post');
+  const filme = await Filme.create(req.body);
+  res.status(201).json(filme)
+  }
+);
+
+
 router.put('/:id', async (req, res) => {
   const [u] = await Filme.update(req.body, { where: { id: req.params.id } });
   if (u) res.json(await Filme.findByPk(req.params.id));
   else  res.status(404).json({ error: 'Filme não encontrado' });
 });
+
 router.delete('/:id', async (req, res) => {
   const d = await Filme.destroy({ where: { id: req.params.id } });
   d ? res.json({ message: 'Deletado' }) : res.status(404).json({ error: 'Não encontrado' });
